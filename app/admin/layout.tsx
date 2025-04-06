3bb8403ca2d7dd244c3982e9d8743b4c11744d5c
PPlada <jubjibdad17@gmail.com>
2025-04-06 16:54:29 +1000

    fixed

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,18 +1,26 @@
 'use client'
+import { ReactNode } from 'react';
 import AdminNavbar from '../components/admin/AdminNavbar'
 import AdminSidebar from '../components/admin/AdminSidebar'
 import AdminFooter from '../components/admin/AdminFooter'
 
-export default function AdminLayout({ children, contents }) {
+interface AdminLayoutProps {
+  children: ReactNode;
+  contents: any;
+}
+
+export default function AdminLayout({ children, contents }: AdminLayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <AdminNavbar />
-      <div className="flex">
-        <AdminSidebar contents={contents} />
-        <main className="flex-1 p-8">
-          {children}
-          <AdminFooter />
-        </main>
+    <div className="min-h-screen bg-gray-50">
+      <div className="py-4">
+        <AdminNavbar />
+        <div className="flex">
+          <AdminSidebar contents={contents} />
+          <main className="flex-1 p-8">
+            {children}
+            <AdminFooter />
+          </main>
+        </div>
       </div>
     </div>
   )
