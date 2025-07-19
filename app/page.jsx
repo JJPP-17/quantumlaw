d4bed2bbd7b94b1268b195789bbec9b9e2431d6b
JJPP-17 <147725534+JJPP-17@users.noreply.github.com>
2025-07-19 18:33:22 +1000

    analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,21 @@
 import HomePage from './home/page';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+       <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KZ1WBWYXWB"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KZ1WBWYXWB');
+          `,
+        }} />
+      </Head>
       <HomePage />
     </>
   )
